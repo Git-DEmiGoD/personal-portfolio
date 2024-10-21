e228840b63bbb371c5084944b28b3ad1619e95c8
ujjwaljoshi <ujjwalyagami@gmail.com>
2024-10-21 09:07:49 +0530

    fixes for deployment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import routesMapping from './routes/routesMapping';
 
 function App() {
   return (
-    <Router basename='/personal-portfolio'>
+    <Router basename={process.env.NODE_ENV === 'production' ? '/personal-portfolio' : '/'}>
         <Routes>
           {routesMapping.map((route, index) => (
             <Route key={index} path={route.path} element={<route.component />} />
